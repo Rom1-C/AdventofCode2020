2a7f1b71e23f4233c984beb6121a23763342916f
Shadowwws <57865000+Rom1-C@users.noreply.github.com>
2020-12-26 20:47:19 +0100

    Update Day25.cpp

--- Day25/Day25.cpp
+++ Day25/Day25.cpp
@@ -4,16 +4,30 @@
 #include <chrono>
 #include <fstream>
 #include <string>
+#include <math.h>
+#include <map>
 
 #define ull unsigned long long
 
 using namespace std;
 using namespace std::chrono;
 
+ull modulo(ull a, ull b, ull n){
+    long long x=1, y=a; 
+    while (b > 0) {
+        if (b%2 == 1) {
+            x = (x*y) % n;
+        }
+        y = (y*y) % n;
+        b = b >> 1;
+    }
+    return x % n;
+}
+
 int main() {
 	auto start = high_resolution_clock::now();
-    int x;
-    int y;
+    ull x;
+    ull y;
     ifstream in;
     in.open("Day25.txt");
     string input;
@@ -21,20 +35,34 @@ int main() {
     x = atoi(input.c_str());
     getline(in,input);
     y = atoi(input.c_str());
-    ull value = 1;
-    ull result = 1;
-    while (value != x){
-        value*=7;
-        value%=20201227;
-        result*=y;
-        result%=20201227;
+    map<ull,ull> alph;
+    ull m = ceil(sqrt(20201227));
+    ull result;
+    for (int i = 0 ; i < m ; i++){
+        alph.insert({modulo(7,i,20201227),i});
+    }
+    for (int i = 0 ; i < m ; i++){
+        alph[modulo(7,i,20201227)] = i;
+    }
+    ull inv = modulo(7,20201225*m,20201227);
+    ull res=0;
+    for (int i = 0 ; i < m ; i++){
+        ull y = (x * modulo(inv,i,20201227))%20201227;
+        auto it = alph.find(y);
+        if (it!=alph.end()){
+            res = i*m + it->second;
+            break;
+        }
     }
 
-    cout << "Day 25 : " << result << endl;
-    
-
+    result = modulo(y,res,20201227);
 	auto stop = high_resolution_clock::now(); 
+
+    cout << "Day 25 : " << result << endl;
+
     auto duration = duration_cast<microseconds>(stop - start);
     std::cout << "Time taken by program: "
          << duration.count() << " microseconds" << endl;
+
+    return 0;
 }
